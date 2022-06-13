3ea6ff69065b5a737fd03e3cafb957d79f4a6f1a
Martin <mr.mmaurer+work@gmail.com>
2022-06-13 22:04:01 +0100

    Added prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,56 +1,56 @@
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:react-hooks/recommended',
     // "plugin:unicorn/recommended",
-    "airbnb",
-    "airbnb-typescript",
+    'airbnb',
+    'airbnb-typescript',
   ],
   env: {
     browser: true,
     es2020: true,
     jest: true,
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    sourceType: "module",
-    project: ["tsconfig.json"],
+    sourceType: 'module',
+    project: ['tsconfig.json'],
     tsconfigRootDir: __dirname,
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   rules: {
-    "jsx-a11y/label-has-for": [
-      "warn",
+    'jsx-a11y/label-has-for': [
+      'warn',
       {
         required: {
-          some: ["nesting", "id"],
+          some: ['nesting', 'id'],
         },
       },
     ],
-    "jsx-a11y/label-has-associated-control": [
-      "warn",
+    'jsx-a11y/label-has-associated-control': [
+      'warn',
       {
         required: {
-          some: ["nesting", "id"],
+          some: ['nesting', 'id'],
         },
       },
     ],
-    "object-curly-newline": [
-      "warn",
+    'object-curly-newline': [
+      'warn',
       {
-        ObjectExpression: "always",
+        ObjectExpression: 'always',
         ObjectPattern: { multiline: true },
-        ImportDeclaration: "never",
+        ImportDeclaration: 'never',
         ExportDeclaration: { multiline: true, minProperties: 3 },
       },
     ],
-    "import/no-extraneous-dependencies": "off",
-    "react/jsx-no-bind": "off", // temp
-    "@typescript-eslint/ban-ts-comment": "off", // temp
-    "no-restricted-exports": "off", // https://github.com/airbnb/javascript/issues/2500
+    'import/no-extraneous-dependencies': 'off',
+    'react/jsx-no-bind': 'off', // temp
+    '@typescript-eslint/ban-ts-comment': 'off', // temp
+    'no-restricted-exports': 'off', // https://github.com/airbnb/javascript/issues/2500
   },
 };
