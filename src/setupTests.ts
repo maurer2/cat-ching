a43262af27d414a7eed810221243207636f80d63
Martin <mr.mmaurer+work@gmail.com>
2022-07-03 21:20:45 +0100

    Added replacement for @babel/preset-env and removed unneeded dependencies and added code coverage tracking

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,2 +1,1 @@
-import 'jest-dom/extend-expect';
 import '@testing-library/jest-dom';
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 import checker from 'vite-plugin-checker';
 import sassDts from 'vite-plugin-sass-dts';
 import eslint from 'vite-plugin-eslint';
+import legacy from '@vitejs/plugin-legacy';
 import path from 'node:path';
 
 export default ({ mode }) => defineConfig({
@@ -21,6 +22,7 @@ export default ({ mode }) => defineConfig({
         outFile: path.resolve(__dirname, './src/style.d.ts'),
       },
     }),
+    legacy(),
   ],
   define: {
     'process.env.NODE_ENV': `"${mode}"`,
@@ -28,5 +30,10 @@ export default ({ mode }) => defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    setupFiles: 'src/setupTests.ts',
+    clearMocks: true,
+    coverage: {
+      reporter: ['text', 'lcov'],
+    },
   },
 });
