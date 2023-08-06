b37d9c0395b212375373334f27c027609594ca34
Martin <mr.mmaurer@gmail.com>
2023-08-06 02:04:14 +0100

    Removed static data in favour of queried data

--- src/components/AppContainer/AppContainer.tsx
+++ src/components/AppContainer/AppContainer.tsx
@@ -1,46 +1,59 @@
-import React, { useEffect } from 'react';
-import { initClient } from '@ts-rest/core';
+import React from 'react';
+import { initQueryClient } from '@ts-rest/react-query';
 
 import { contract } from '../../../server/contract';
 import App from '../App';
 import Money from '../../types/Money';
-import coinData from '../../data/coins';
 import type { Coin } from '../../types/Coin';
 
 const port = import.meta.env.VITE_SERVER_PORT;
+const client = initQueryClient(contract, {
+  baseUrl: `http://localhost:${port}`,
+  baseHeaders: {},
+});
 
 function AppContainer() {
-  useEffect(() => {
-    const client = initClient(contract, {
-      baseUrl: `http://localhost:${port}`,
-      baseHeaders: {},
+  const { data, isLoading, error } = client.getCoins.useQuery(
+    ['coins'],
+    {},
+    { staleTime: Number.POSITIVE_INFINITY },
+  );
+
+  // eslint-disable-next-line unicorn/no-null
+  let coinList: ReadonlyArray<Coin> | null = null;
+  if (data?.status && data?.body !== null) {
+    coinList = data.body.map((coin) => {
+      const {
+        name,
+        image,
+        size,
+        value,
+      } = coin;
+
+      return {
+        name,
+        image,
+        size,
+        amount: Money.fromNumber(value, 'GBP'),
+      };
     });
+  }
 
-    client.getCoins({})
-      .then(({ body, status }) => {
-        if (status === 200 && body !== null) {
-          console.log(body);
-        }
-      });
-  }, []);
+  // todo styling
+  if (error) {
+    return (
+      <div role="status">
+        Error
+      </div>
+    );
+  }
 
-  const coinList: ReadonlyArray<Coin> = coinData.map((coin) => {
-    const {
-      name,
-      image,
-      size,
-      value,
-    } = coin;
-
-    return {
-      name,
-      image,
-      size,
-      amount: Money.fromNumber(value, 'GBP'),
-    };
-  });
-
-  return (
+  // todo styling
+  return (isLoading || coinList === null) ? (
+    <div role="status">
+      Loading
+    </div>
+  ) : (
     <App coinList={coinList} data-testid="app" />
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import './index.css';
 import AppContainer from './components/AppContainer';
@@ -9,9 +10,13 @@ if (!container) {
   throw new Error('root element can\'t be found');
 }
 
+const queryClient = new QueryClient();
+
 const root = createRoot(container);
 root.render(
   <StrictMode>
-    <AppContainer />
+    <QueryClientProvider client={queryClient}>
+      <AppContainer />
+    </QueryClientProvider>
   </StrictMode>,
 );
