afe62c320d30684275e486cd3f646189b551c731
Martin <mr.mmaurer@gmail.com>
2023-08-15 01:21:33 +0100

    Added error message from server contract to error overlay

--- server/app.ts
+++ server/app.ts
@@ -51,6 +51,7 @@ const router = server.router(contract, {
   getCoins: async () => {
     const coinList = [...coins satisfies readonly CoinData[]];
 
+    /// if (coinList.length === 0) {
     if (coinList.length > 0) {
       return {
         status: 200,
